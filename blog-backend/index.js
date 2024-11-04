e81d51aba4892db55e2827c9b4a498287c219aaa
Isabella <isabellaotoo25@gmail.com>
2024-11-04 10:15:43 +0000

    Update index.js

--- blog-backend/index.js
+++ blog-backend/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -15,6 +15,9 @@ app.use(bodyParser.json());
 const postsRoute = require('./routes/posts');
 app.use('/posts', postsRoute);
 
+const mongoURI = process.env.MONGO_URI;
+
+console.log('MongoDB URI:', mongoURI);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
